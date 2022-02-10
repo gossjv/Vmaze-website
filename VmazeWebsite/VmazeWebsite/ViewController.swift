5bbc61f6644c56c236fc4db82225e51707e2bae2
Jorge Vasquez <jorgervasquez@gmail.com>
2022-02-10 12:52:55 -0300

    Project creation

---
+++ VmazeWebsite/VmazeWebsite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VmazeWebsite
+//
+//  Created by Pedro Valderrama on 10/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
